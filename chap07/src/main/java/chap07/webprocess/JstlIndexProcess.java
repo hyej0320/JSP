2bf3b9f15a042ee18a5151ff251d47d5d570abfb
Hyejin <hyej0320@gmail.com>
2024-07-31 16:24:31 +0900

    update

--- chap07/src/main/java/chap07/webprocess/JstlIndexProcess.java
+++ chap07/src/main/java/chap07/webprocess/JstlIndexProcess.java
@@ -1,5 +1,8 @@
 package chap07.webprocess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +16,18 @@ public class JstlIndexProcess implements WebProcess{
 		request.setAttribute("animals", new String[] {"강아지", "고양이", "호랑이", "여우", "토끼", "소", "닭"});
 		
 		
+		List<String> fruits = new ArrayList<>(); 
+		request.setAttribute("fruits", fruits);
+		
+		fruits.add("사과");
+		fruits.add("오렌지");
+		fruits.add("복숭아");
+		fruits.add("메론");
+		fruits.add("체리");
+		fruits.add("수박");
+		fruits.add("참외");
+		fruits.add("딸기");
+
 		return "/jstl/index";
 	}
 }
